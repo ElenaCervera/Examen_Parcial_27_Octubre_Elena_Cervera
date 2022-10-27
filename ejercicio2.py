d160599a108a0da01c29d163f5e9fe791c36187e
ElenaCervera <elena.cerveralopez@usp.ceu.es>
2022-10-27 20:55:44 +0200

    commit 9

--- ejercicio2.py
+++ ejercicio2.py
@@ -10,19 +10,39 @@
 lista=[18, 50, 210, 80, 145, 333, 70, 30]
 
 for i in lista:
-    if i >300:
-        break
-    else:
-        if i%10==0 and i< 200:
-            print(i)
-def megesort(lista):
-    if len(l)<=1:
+  if i >300:
+     break
+  else:
+         if i%10==0 and i< 200:
+             print(i)
+             
+def mergesort(lista):
+    if len(lista)<=1:
         return lista
     else:
         mitad=len(lista)//2 
         izq=[]
         for i in range(0,mitad):
             izq.append(lista[i])
+        dcha=[]
+        for i in range(mitad, len(lista)):
+            dcha.append(lista[i])
+        dcha=mergesort(dcha)
+        izq=mergesort(izq)
+        if (izq[mitad-1]<= dcha[0]):
+            izq+= dcha
+            return izq
+        resultado=mergesort(izq, dcha)
+        return resultado
+def devolver(indice):
+    for i in resultado:
+        if i == indice:
+            return indice
+        else:
+            return -1
+         
+
+
 
 
 
